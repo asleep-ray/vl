8e6636e96ee5aaf9fc0f45a3f433c70c615ad25c
asleep-ray <wakeup@asleep.ai>
2024-05-21 00:00:32 +0900

    Refactor model and train part

--- model.py
+++ model.py
@@ -1,56 +1,67 @@
 import torch
-
-# from peft import LoraConfig, get_peft_model
-# from PIL import Image
+from peft import LoraConfig, get_peft_model
 from torch import nn
 from torch.nn import CrossEntropyLoss
 from transformers import CLIPModel, T5ForConditionalGeneration, T5Tokenizer
 from transformers.generation_utils import GenerationMixin
 from transformers.modeling_outputs import Seq2SeqLMOutput
 
+FLAN_T5_BASE = "google/flan-t5-base"
+CLIP_VIT_BASE_PATCH16 = "openai/clip-vit-base-patch16"
+
 
 class IntegratedModel(nn.Module, GenerationMixin):
-    def __init__(self, model_name="google/flan-t5-base"):
+    def __init__(self):
         super(IntegratedModel, self).__init__()
-        self.tokenizer = T5Tokenizer.from_pretrained(model_name)
-        # Add a custom start token if not already present
-        start_token = "<s>"
-        self.tokenizer.get_vocab()
-        if start_token not in self.tokenizer.get_vocab():
-            self.tokenizer.add_special_tokens({"bos_token": start_token})
 
-        self.model = T5ForConditionalGeneration.from_pretrained(model_name)
+        self.encoder = CLIPModel.from_pretrained(CLIP_VIT_BASE_PATCH16)
 
-        self.decoder = self.model.decoder
-        self.lm_head = self.model.lm_head
-        self.config = self.model.config
+        flan_t5_model = T5ForConditionalGeneration.from_pretrained(FLAN_T5_BASE)
+        self.decoder = flan_t5_model.decoder
+        self.lm_head = flan_t5_model.lm_head
 
-        self.encoder = CLIPModel.from_pretrained("openai/clip-vit-base-patch16")
         # Freeze the encoder parameters
         for param in self.encoder.parameters():
             param.requires_grad = False
 
-    def forward(self, processed_image, attention_mask=None, labels=None, **kwargs):
-        has_value = any(value is not None for value in kwargs.values())
-        if has_value:
-            raise ValueError("This model does not support extra kwargs.")
+        lora_config = LoraConfig(
+            r=8, lora_alpha=32, target_modules=["q", "v", "wi_0", "wi_1"], lora_dropout=0.05, bias="none"
+        )
+        self.decoder = get_peft_model(self.decoder, lora_config)
 
-        processed_image = processed_image.squeeze(1)
-        # Encode the input text
+        self.tokenizer = T5Tokenizer.from_pretrained(FLAN_T5_BASE)
+        self.tokenizer.add_special_tokens({"bos_token": "<s>"})  # Add a custom start token if not already present
+
+    def get_encoder_output(self, processed_image: torch.Tensor) -> torch.Tensor:
+        """Get the encoded representation of the input image.
+
+        Args:
+            processed_image: The processed image tensor with shape of (bs, 3, 244, 244).
+
+        Returns:
+            encoder_outputs: The encoded representation of the input image with shape of (bs, 196, 768).
+        """
         outputs = self.encoder.vision_model(processed_image)
+        patch_embeddings = outputs.last_hidden_state  # shape (bs, 197, 768), patch embeddings are in the hidden_states
+        patch_embeddings = patch_embeddings[:, 1:, :]  # shape (bs, 196, 768), Exclude the CLS token embedding
+        encoder_outputs = patch_embeddings / patch_embeddings.norm(p=2, dim=-1, keepdim=True)  # Normalize embeddings
+        return encoder_outputs
 
-        # The patch embeddings are located in the hidden_states of the model output
-        patch_embeddings = outputs.last_hidden_state
+    def forward(self, processed_image, attention_mask=None, labels=None, **kwargs):
+        """Forward pass of the model.
 
-        # Exclude the CLS token embedding
-        patch_embeddings = patch_embeddings[:, 1:, :]
+        Args:
+            processed_image: Processed image tensor with shape (bs, 1, 3, 244, 244). TODO: Change to (bs, 3, 244, 244)
+            attention_mask: The attention mask tensor.
+            labels: The labels tensor with shape of (bs, seq_len).
 
-        # Normalize the embeddings
-        encoder_outputs = patch_embeddings / patch_embeddings.norm(p=2, dim=-1, keepdim=True)
+        Returns:
+            Seq2SeqLMOutput: The output of the model.
+        """
+        processed_image = processed_image.squeeze(1)  # shape (bs, 1, 3, 244, 244) -> (bs, 3, 244, 244)
+        encoder_outputs = self.get_encoder_output(processed_image)  # shape (bs, 196, 768)
 
-        # Shift the labels to the right for decoder input
-        decoder_input_ids = self._shift_right(labels)
-
+        decoder_input_ids = self._shift_right(labels)  # Shift the labels to the right for decoder input
         # Decode the encoded representation
         decoder_outputs = self.decoder(
             input_ids=decoder_input_ids, encoder_hidden_states=encoder_outputs, encoder_attention_mask=attention_mask
@@ -67,7 +78,6 @@ class IntegratedModel(nn.Module, GenerationMixin):
             loss_fct = CrossEntropyLoss(ignore_index=self.tokenizer.pad_token_id)
             labels = labels.to(lm_logits.device)  # move labels to correct device to enable PP
             loss = loss_fct(lm_logits.view(-1, lm_logits.size(-1)), labels.view(-1))
-            # TODO(thom): Add z_loss https://github.com/tensorflow/mesh/blob/fa19d69eafc9a482aff0b59ddd96b025c0cb207d/mesh_tensorflow/layers.py#L666
 
         return Seq2SeqLMOutput(
             loss=loss,
@@ -80,52 +90,34 @@ class IntegratedModel(nn.Module, GenerationMixin):
 
     def _shift_right(self, input_ids):
         decoder_start_token_id = self.tokenizer.bos_token_id
-        pad_token_id = 0
 
         shifted_input_ids = input_ids.new_zeros(input_ids.shape)
         shifted_input_ids[..., 1:] = input_ids[..., :-1].clone()
         shifted_input_ids[..., 0] = decoder_start_token_id
 
-        # shifted_input_ids.masked_fill_(shifted_input_ids == -100, pad_token_id)
-
         return shifted_input_ids
 
     def generate(self, processed_image, max_new_tokens=50):
+        """Generate text from the input image.
 
-        # Encode the image
-        outputs = self.encoder.vision_model(processed_image)
+        Args:
+            processed_image: The processed image tensor with shape of (bs, 3, 244, 244).
+            max_new_tokens: The maximum number of tokens to generate.
 
-        # The patch embeddings are located in the hidden_states of the model output
-        patch_embeddings = outputs.last_hidden_state
-
-        # Exclude the CLS token embedding
-        patch_embeddings = patch_embeddings[:, 1:, :]
-
-        # Normalize the embeddings
-        encoder_outputs = patch_embeddings / patch_embeddings.norm(p=2, dim=-1, keepdim=True)
-
-        # Initialize decoder input IDs with the start token
-        decoder_input_ids = torch.tensor([self.tokenizer.bos_token_id]).unsqueeze(0)
-
-        # Greedily generate text
-        generated_ids = decoder_input_ids
+        returns:
+            The generated token ids.
+        """
+        encoder_outputs = self.get_encoder_output(processed_image)
+        generated_ids = torch.tensor([self.tokenizer.bos_token_id]).unsqueeze(0)  # Initialize with the start token
 
         for _ in range(max_new_tokens):
-            # Decode the encoded representation
             decoder_outputs = self.decoder(input_ids=generated_ids, encoder_hidden_states=encoder_outputs)
-
-            # Get the logits for the next token
             sequence_output = decoder_outputs.last_hidden_state
             next_token_logits = self.lm_head(sequence_output[:, -1, :])
-
-            # Greedily select the token with the highest probability
-            next_token_id = next_token_logits.argmax(dim=-1).unsqueeze(-1)
-
-            # Append the predicted token ID to the generated sequence
+            next_token_id = next_token_logits.argmax(dim=-1).unsqueeze(-1)  # Greedily select the token
             generated_ids = torch.cat([generated_ids, next_token_id], dim=-1)
 
-            # If the end-of-sequence token is generated, stop
-            if next_token_id == self.tokenizer.eos_token_id:
+            if next_token_id == self.tokenizer.eos_token_id:  # Stop when end-of-sequence token is generated
                 break
 
-        return generated_ids, encoder_outputs
+        return generated_ids
--- train.py
+++ train.py
@@ -4,7 +4,6 @@ from datetime import datetime
 import pytz
 import torch
 from datasets import load_from_disk
-from peft import LoraConfig, get_peft_model
 from transformers import Trainer, TrainingArguments
 
 import model
@@ -19,39 +18,27 @@ os.environ["DISABLE_MLFLOW_INTEGRATION"] = "TRUE"
 dataset_dict = load_from_disk("dataset/small_split")
 
 # Load the model
-model_name = "google/flan-t5-base"
-integrated_model = model.IntegratedModel(model_name)
+integrated_model = model.IntegratedModel()
 print("integrated_model is loaded")
 utils.print_trainable_model_module(integrated_model)
 
-
-lora_config = LoraConfig(
-    r=8,  # Rank
-    lora_alpha=32,
-    target_modules=["q", "v", "wi_0", "wi_1"],
-    lora_dropout=0.05,
-    bias="none",
-)
-integrated_model.decoder = get_peft_model(integrated_model.decoder, lora_config)
-print("lora is applied to the decoder of the model.")
-utils.print_trainable_model_module(integrated_model)
-
 output_dir = f"./output/integrated_model-small_split-{timestamp}"
-
 print(f"Output directory: {output_dir}")
 
 training_args = TrainingArguments(
     output_dir=output_dir,
-    learning_rate=1e-3,
-    num_train_epochs=20,
-    weight_decay=0.01,
+    learning_rate=1e-2,
+    num_train_epochs=15,
+    weight_decay=0.03,
     logging_steps=1,
-    max_steps=500,
+    # max_steps=500,
     logging_dir=f"./runs/{timestamp}",
     save_steps=50,
     save_total_limit=5,
     per_device_train_batch_size=16,  # Increase the batch size for training
     per_device_eval_batch_size=8,  # Increase the batch size for evaluation
+    evaluation_strategy="steps",
+    eval_steps=50,
 )
 
 trainer = Trainer(
@@ -71,5 +58,4 @@ def save_model_custom(output_dir, **kwargs):
 
 
 trainer.save_model = save_model_custom
-
 trainer.train()
